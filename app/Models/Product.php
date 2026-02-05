952d43f5fb0388aa035e2136a92690fedc7dafeb
ursgarsan <ugsugs333@gmail.com>
2026-02-05 15:04:02 +0100

    FEAT: softdelete implementado

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'products';
     protected $fillable = ['name', 'price', 'description', 'stock', 'image', 'inactive', 'categoryId', 'supplierId'];
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
+    const DELETED_AT = 'deletedAt';
 
     public function category()
     {
