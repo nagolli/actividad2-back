67ba14da67a38e6553c512fe3763e47cce52bb73
ursgarsan <ugsugs333@gmail.com>
2026-02-11 18:29:08 +0100

    FEAT: endpoint nota media

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,7 @@ Route::put('order/{id}', [Order::class, 'update']);
 Route::delete('order/{id}', [Order::class, 'destroy']);
 
 Route::get('review', [Review::class, 'index']);
+Route::get('review/{productId}/average-rating', [Review::class, 'averageRating']);
 Route::get('review/{productId}/{email}', [Review::class, 'show']);
 Route::post('review', [Review::class, 'store']);
 Route::put('review/{productId}/{email}', [Review::class, 'update']);
